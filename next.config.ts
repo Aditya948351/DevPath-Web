70ca1dea0cdf7044087e8f490a0520d9d70bac4d
Aditya Akolkar <aditya.akolkar.here@gmail.com>
2025-12-14 23:46:10 +0530

    chore: configure github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: {
+    // @ts-ignore - buildActivity is valid but missing in type definition
     buildActivity: false,
+    // @ts-ignore - appIsrStatus is valid but missing in type definition
     appIsrStatus: false,
   },
   reactCompiler: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
