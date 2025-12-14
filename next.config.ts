87c6c2adddb0f3d353e4d57a7accd1bb834b1de0
Aditya Akolkar <aditya.akolkar.here@gmail.com>
2025-12-15 00:19:25 +0530

    fix: configure basePath for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
+  basePath: isProd ? "/DevPath" : "",
   /* config options here */
   devIndicators: {
     // @ts-ignore - buildActivity is valid but missing in type definition
